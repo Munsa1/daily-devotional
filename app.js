b9cb3335bf83e56cb7cf472dd0fac89c1625a352
Munsa1 <munsamibenge65@gmail.com>
2024-09-27 14:08:01 +0200

    Add devotional text and socials content to site

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 
 
-let currentDate = moment().format("MMMM Do YYYY");
+let currentDate = moment().format("MMMM DD YYYY");
 
 document.getElementById('devotionDate').innerText = currentDate;
 
-
+fetch('https://bible-api.com/john3:16')
+.then(res =>{
+    return res.json();
+    
+})
+.then(data =>{
+    console.log(data);
+    
+})
+.catch(error => console.log(error));
 
 
 
