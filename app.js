2bc39504a08eca8d38a6d5582906ad118cd48a34
Munsa1 <munsamibenge65@gmail.com>
2024-09-27 16:25:20 +0200

    consume bible verse via API

--- app.js
+++ app.js
@@ -10,11 +10,27 @@ fetch('https://bible-api.com/john3:16')
     
 })
 .then(data =>{
-    console.log(data);
+
+    const verse = data.text;
+    const verseBin = document.getElementById('verseText');
+    verseBin.innerText = verse;
+
+    
     
 })
 .catch(error => console.log(error));
 
+
+// const verse = document.getElementById('verse');
+
+// verse.addEventListener('click', ()=>{
+//     data.forEach(verse =>{
+//         const markup = `<p>${verse.text}</p>`;
+//         document.querySelector('pop-up-verse').insertAdjacentHTML('beforeend', markup);
+
+//     })
+    
+// })
 
 
 
