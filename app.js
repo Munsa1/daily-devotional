f01807b5967546eb300aa279320720d53c3c808d
Munsa1 <munsamibenge65@gmail.com>
2024-10-08 19:54:03 +0200

    Add desktop mobile view fix consisten on popup

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 
 // Add current date
 let currentDate = moment().format("MMMM DD YYYY");
-
 document.getElementById('devotionDate').innerText = currentDate;
 
 
@@ -9,21 +8,14 @@ document.getElementById('devotionDate').innerText = currentDate;
 // Fetch verse from API
 fetch('https://bible-api.com/john3:16')
 .then(res =>{
-    return res.json();
-    
+    return res.json();    
 })
 .then(data =>{
-
     const verse = data.text;
     const verseBin = document.getElementById('verseText');
-    verseBin.innerText = verse;
-
-    
-    
+    verseBin.innerText = verse;   
 })
 .catch(error => console.log(error));
-
-
 
 
 
